2f4eea9d18fda9a1d841d64f5bf9ca66747e83f7
原科愛子 <aiko@harashinaaiko-no-MacBook-Pro.local>
2016-07-20 15:09:49 -0400

    July 20.

---
+++ chat/chat_server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var chatArchive = [];
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "OPTIONS,HEAD,GET,PUT");
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+io.on('connection', function(socket){
+  console.log("New Client Connection");
+  socket.on("chat message", function(msg){
+    chatArchive.push(msg);
+    io.emit('chat message', msg);
+
+  });
+});
+
+app.get('/get_archive', function(req,res){
+  res.send(chatArchive);
+
+});
+
+http.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
